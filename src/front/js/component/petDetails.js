3b249128e7dcb9ac2b7afa7be1ead2b9c0b9bed8
gleonag <gleonag@gmail.com>
2023-01-20 19:31:03 +0000

    cards

--- src/front/js/component/petDetails.js
+++ src/front/js/component/petDetails.js
@@ -8,21 +8,27 @@ const PetDetails = (props) => {
     
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
-        <div className="petList container m-0 p-0">
-            <div key={props.id} className="PetDetails card mobile-space">
-            <img className="img-design" src={props.img} alt="Card image cap"></img>
-            <div className="card-body pet-card-details py-4">
-                <h4 className="card-title pt-3 pb-4"><strong>{props.title}</strong></h4>
-                <p className="card-text"><b>Id:</b> {props.id}</p>
-                <p className="card-text"><b>Age:</b> {props.age}</p>
-                <p className="card-text"><b>Date of Birth: </b>{props.birth}</p>
-                <p className="card-text"><b>Colour:</b> {props.colour}</p>
-                
-            </div>
-            <div className="py-4 pet-card-button">
-                <a href={props.buttonUrl} className="btn btn-outline-primary p-2">{props.buttonLabel}</a>
+        <div className="container">
+            
+            <div className="card text-center shadow">
+            
+                <div key={props.id} className="card mobile-space">
+                    <div className="overflow">
+                        <img className="card-img-top" src={props.img} alt="Card image cap"></img>
+                    </div>
+                    <div className="card-body text-dark">
+                        <h4 className="card-title"><strong>{props.title}</strong></h4>
+                        <p className="card-text"><b>Id:</b> {props.id}</p>
+                        <p className="card-text"><b>Age:</b> {props.age}</p>
+                        <p className="card-text"><b>Date of Birth: </b>{props.birth}</p>
+                        <p className="card-text"><b>Colour:</b> {props.colour}</p>     
+                    </div>
+                <div className="card-button py-1">
+                    <a href={props.buttonUrl} className="button btn btn-outline p-2">{props.buttonLabel}</a>
+                    </div>
                 </div>
-            </div>
+            </div>    
+        
     </div>
 
 
